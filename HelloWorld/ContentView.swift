8f7f810e5265762e51f16eebd11c7ed29cc07a21
Yongjae Lim <yongjae@vt.edu>
2022-10-05 01:20:08 -0700

    [10/04/2022] iOS review - Configuring views - View modifiers - some View syntax (Opaque type)

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack { // background view
+            Color.gray.opacity(0.1).edgesIgnoringSafeArea(.all)
+            
+            VStack { // foreground view
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+                    .font(.largeTitle)
+                    .padding(.bottom)
+                HStack {
+                    Image(systemName: "star") // SF symbols
+                    Text("Yongjae is going to be the best engineer")
+                        .font(.system(size: 16, weight: .bold, design: .serif))
+                        .foregroundColor(.blue)
+                        .multilineTextAlignment(.center)
+                    Image(systemName: "star.fill")
+                } // HStack
+            } // VStack
+            .padding()
+        } // ZStack
     }
 }
 
