8b3ed16d32eb1c55fbe65768d18b75ab92c688c4
Yongjae Lim <yongjae@vt.edu>
2022-10-04 23:42:14 -0700

    Initial Commit

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Yongjae Lim on 10/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Yongjae Lim on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
